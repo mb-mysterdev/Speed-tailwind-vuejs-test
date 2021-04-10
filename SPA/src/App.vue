2ddf0dd9ee55eb9a2223a3b96e22f70e22c1a78c
B M <mohamed.bouhleel@gmail.com>
2021-04-10 13:25:41 +0200

    install vuejs + element ui

--- SPA/src/App.vue
+++ SPA/src/App.vue
@@ -1,19 +1,37 @@
 <template>
-  <v-app>
-    <app-front>
-
-    </app-front>
-    <router-view></router-view>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <el-tag>Tag 1</el-tag>
+      <el-tag type="success">Tag 2</el-tag>
+      <el-tag type="info">Tag 3</el-tag>
+      <el-tag type="warning">Tag 4</el-tag>
+      <el-tag type="danger">Tag 5</el-tag>
+    </div>
+    <router-view />
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import VueRouter from "vue-router/types/router";
-import AppFront from "@/components/base/AppFront.vue";
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default Vue.extend({
-  name: 'App',
-  components: {AppFront},
-});
-</script>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
